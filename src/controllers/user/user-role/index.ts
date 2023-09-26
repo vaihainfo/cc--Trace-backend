ef794161e54269bf3cf1dfcd22fb3260c536b03b
root <root@162-215-14-174.webhostbox.net>
2023-09-26 03:41:23 -0600

    latest changes

--- src/controllers/user/user-role/index.ts
+++ src/controllers/user/user-role/index.ts
@@ -146,14 +146,12 @@ const updateUserRole = async (req: Request, res: Response) => {
         }
         const roleId = req.body.id;
         const updatedData = {
-            brand_id: req.body.brandId,
+            brand_id: req.body.brandId ? req.body.brandId : undefined,
             user_role: req.body.userRole
         };
 
-        const privilegesToUpdate = [];
-        const privilegesToInsert = [];
-
-        for (const privilege of req.body.privileges) {
+        
+        for await (const privilege of req.body.privileges) {
             const existingPrivilege = await UserPrivilege.findOne({
                 where: {
                     userRole_id: roleId,
@@ -161,9 +159,10 @@ const updateUserRole = async (req: Request, res: Response) => {
                 },
             });
 
+            console.log(existingPrivilege)
+
             if (existingPrivilege) {
-                privilegesToUpdate.push({
-                    id: existingPrivilege.id,
+                let update = await UserPrivilege.update({
                     userRole_id: roleId,
                     menu_id: privilege.menuId,
                     create_privilege: privilege.create,
@@ -171,9 +170,9 @@ const updateUserRole = async (req: Request, res: Response) => {
                     edit_privilege: privilege.edit,
                     delete_privilege: privilege.delete,
                     status: existingPrivilege.status
-                });
+                }, { where: { id: existingPrivilege.id } })
             } else {
-                privilegesToInsert.push({
+                let create = await UserPrivilege.create({
                     userRole_id: roleId,
                     menu_id: privilege.menuId,
                     create_privilege: privilege.create,
@@ -181,26 +180,22 @@ const updateUserRole = async (req: Request, res: Response) => {
                     edit_privilege: privilege.edit,
                     delete_privilege: privilege.delete,
                     status: true
-                });
+                })
             }
         }
-
-        const [rowsUpdated, [updatedRole]] = await Promise.all([
+        const rowsUpdated = await
             UserRole.update(updatedData, {
                 where: { id: roleId },
                 returning: true,
-            }),
-            UserPrivilege.bulkCreate(privilegesToUpdate, {
-                updateOnDuplicate: ['create_privilege', 'view_privilege', 'edit_privilege', 'delete_privilege'],
-            }),
-            UserPrivilege.bulkCreate(privilegesToInsert),
-        ]);
+            })
+
+
 
         if (rowsUpdated === 0) {
             return res.sendError(res, "ERR_ROLE_NOT_FOUND");
         }
 
-            return res.sendSuccess(res, updatedRole);
+         return res.sendSuccess(res, rowsUpdated);
       }catch (error) {
           console.log(error)
           return res.sendError(res, "ERR_ROLE_NOT_UPDATED");
