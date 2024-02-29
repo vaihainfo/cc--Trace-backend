b8dea7545c07b31e79e40bb357d2269ff20cea9e
satya <sb4singh@gmail.com>
2024-02-29 15:41:43 +0530

    fix in spinner tracing

--- src/util/tracing-chart-data-formatter.ts
+++ src/util/tracing-chart-data-formatter.ts
@@ -37,32 +37,38 @@ export const formatDataForGinnerProcess = (reelLotNo: any, data: any): any => {
     return treeData;
 }
 
-const getVillagesForGinSales =(sales: [any]) => {
+const getVillagesForGinSales = (sales: any) => {
     let villages = new Set();
-    sales.forEach((el: any) => {
-        el.transaction.forEach((el: any) => {
-            villages.add(el.village.village_name);
-        })
-    });
+    sales.transaction.forEach((el: any) => {
+        el = el.toJSON();
+        villages.add(el.village.village_name);
+    })
     return Array.from(villages);
 }
 
 export const formatDataForSpinnerProcess = (reelLotNo: any, data: any): any => {
+    if (data.length == 0) return {
+        name: reelLotNo,
+        type: 'spinner_image',
+        width: 200,
+        height: 100,
+        isRoot: true,
+    };
     let treeData = {
         name: reelLotNo,
         type: 'spinner_image',
         width: 200,
         height: 100,
         isRoot: true,
-        children: data.map((el: any) => {
+        children: data[0].ginSales.map((el: any) => {
             return {
-                name: el.spinner.name,
+                name: el.ginner.name,
                 type: 'farm',
                 width: 200,
                 height: 40,
                 children: [
                     {
-                        name: el.spinner.name,
+                        name: el.ginner.name,
                         type: 'cotton_image',
                         width: 200,
                         height: 100,
@@ -70,27 +76,27 @@ export const formatDataForSpinnerProcess = (reelLotNo: any, data: any): any => {
                             {
                                 name: 'Ginner',
                                 type: 'ginner',
-                                list: el.ginSales.map((el: any) => el.reel_lot_no),
+                                list: el.reel_lot_no.split(',').map((el: any) => el.trim()),
                                 intro: ``,
                                 width: 200,
-                                height: el.ginSales.map((el: any) => el.reel_lot_no).length * 40,
+                                height: el.reel_lot_no.split(',').length * 40,
                                 children: [
-                                   {
-                                    name: el.spinner.name,
-                                    type: 'village_image',
-                                    width: 50,
-                                    height: 50,
-                                    children: [
-                                        {
-                                            name: 'Village',
-                                            type: 'village',
-                                            list: getVillagesForGinSales(el.ginSales),
-                                            intro: `${getVillagesForGinSales(el.ginSales).length > 1 ? 'Multiple' : 'Single'} Village${getVillagesForGinSales(el.ginSales).length > 1 ? 's' : ''} Seed Cotton Consumption for REEL Bale Lot`,
-                                            width: 200,
-                                            height: getVillagesForGinSales(el.ginSales).length * 40 + 60,
-                                        }   
-                                    ]
-                                   } 
+                                    {
+                                        name: el.ginner.name,
+                                        type: 'village_image',
+                                        width: 50,
+                                        height: 50,
+                                        children: [
+                                            {
+                                                name: 'Village',
+                                                type: 'village',
+                                                list: getVillagesForGinSales(el),
+                                                intro: `${getVillagesForGinSales(el).length > 1 ? 'Multiple' : 'Single'} Village${getVillagesForGinSales(el).length > 1 ? 's' : ''} Seed Cotton Consumption for REEL Bale Lot`,
+                                                width: 200,
+                                                height: getVillagesForGinSales(el).length * 40 + 60,
+                                            }
+                                        ]
+                                    }
                                 ]
                             }
                         ]
