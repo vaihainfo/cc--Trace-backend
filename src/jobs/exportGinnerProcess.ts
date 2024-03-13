cc46f7499405f8dec5157ad5cba2f4843457bd1d
ratech98 <ratech98@gmail.com>
2024-03-13 15:34:31 +0530

    work on to export large file in less a minute

---
+++ src/jobs/exportGinnerProcess.ts
@@ -0,0 +1,9 @@
+// import { exportGinnerProcessSchedule } from "@controllers/reports";
+
+// var cron = require("node-cron");
+
+// cron.schedule("10 * * * * *", async () => {
+//     console.log("entry");
+    
+//   exportGinnerProcessSchedule();
+// });
