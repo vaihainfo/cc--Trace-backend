e2f94b0deaf3109d737493b61373354e21642080
dlaller <dlaller1988@gmail.com>
2024-03-19 10:16:25 +0530

    [added]

---
+++ src/migrations/20240319043949-add-column-export.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('export_ginner_lintbale_sales', 'createdAt', {
+      type: Sequelize.DATE,
+    });
+    await queryInterface.addColumn('export_ginner_lintbale_sales', 'updatedAt', {
+      type: Sequelize.DATE
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('export_ginner_lintbale_sales', 'createdAt');
+    await queryInterface.removeColumn('export_ginner_lintbale_sales', 'updatedAt');
+  }
+};
