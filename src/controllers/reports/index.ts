18e8f6fd981be278ee117ab5bf79d2c9670eef51
dlaller <dlaller1988@gmail.com>
2024-06-05 18:56:50 +0530

    Spin yarn Process Report

--- src/controllers/reports/index.ts
+++ src/controllers/reports/index.ts
@@ -2907,7 +2907,7 @@ const fetchSpinnerYarnProcessPagination = async (
     yarn_count_data AS (
       SELECT
         spin_process.id AS process_id,
-        json_agg(json_build_object('id'::integer, yarn_count.id, 'yarnCount_name'::text, yarn_count."yarnCount_name")) AS yarncount
+        json_agg(json_build_object('id', yarn_count.id, 'yarnCount_name', yarn_count."yarnCount_name")) AS yarncount
       FROM
         spin_processes spin_process
       LEFT JOIN
