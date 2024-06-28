e0b7043f642af92776b53f58e36dee0bcd665de3
prabhu <prabhu@ascent24.io>
2024-06-28 06:19:02 +0000

    issue 10 physical partner fixed

--- src/router/settings/physical-partner.ts
+++ src/router/settings/physical-partner.ts
@@ -4,14 +4,16 @@ import {
     fetchPhysicalPartnerPagination,
     fetchPhysicalPartner,
     deletePhysicalPartner,
-    checkPhysicalPartner
+    checkPhysicalPartner,
+    getPhysicalPartnerBrand
 } from "../../controllers/process-registration/physical-partner";
 
 const router = Router();
 
-router.use(accessControl);
+// router.use(accessControl);
 
 router.get('/', fetchPhysicalPartnerPagination);
+router.get('/get/physicalPartner/brand', getPhysicalPartnerBrand);
 router.get('/get-physical-partner', fetchPhysicalPartner);
 router.delete('/', deletePhysicalPartner);
 router.post('/check-physical-partner', checkPhysicalPartner);
