9178453fdd131bb66c2b50b9ee0368b0a0344b7a
deepaklaller1988 <dlaller1988@gmail.com>
2024-08-06 19:07:18 +0530

    add cron manually 06.08.24

--- src/app.ts
+++ src/app.ts
@@ -117,7 +117,7 @@ const connectToDb = async () => {
   try {
     await sequelize.authenticate();
       console.log("Database Connected successfully.");
-      exportReportsOnebyOne();
+      exportReportsTameTaking();
       const used = process.memoryUsage();
       console.log(`Memory usage: ${JSON.stringify(used)}`);
       console.log("Current Server Time", moment());
