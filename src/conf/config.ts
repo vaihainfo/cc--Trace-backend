ef014ab882d48dca21859e95636aba5e6e89fafd
deepaklaller1988 <dlaller1988@gmail.com>
2024-11-06 18:11:16 +0530

    10 minues pool time

--- src/conf/config.ts
+++ src/conf/config.ts
@@ -7,5 +7,15 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: "postgres"
+  dialect: "postgres",
+  dialectOptions: {
+    connectTimeout: 60000,  // 1 minute for establishing connection
+    statement_timeout: 600000,  // 10 minutes for query execution timeout
+  },
+  pool: {
+    max: 100,  // Increased pool size
+    min: 0,
+    acquire: 600000,  // 10 minutes to acquire a connection from the pool
+    idle: 10000,  // 10 seconds idle timeout
+  },
 }
