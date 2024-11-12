869d286cffb7a9ad971263fb8fbbe24bc1cac13b
padaliadhara <padalia.dhara@gmail.com>
2024-11-12 17:31:21 +0530

    Greyout update status for spinner process and ginner sale

--- src/router/update-greyout/index.ts
+++ src/router/update-greyout/index.ts
@@ -1,4 +1,4 @@
-import { updateGinProcessGreyoutStatusData } from "../../controllers/update-greyout";
+import { updateGinProcessGreyoutStatusData, updateSpinProcessGreyoutStatusData, updateGinSalesGreyoutStatusData } from "../../controllers/update-greyout";
 import accessControl from "../../middleware/access-control";
 import { Router } from "express";
 
@@ -6,5 +6,7 @@ const router = Router();
 
 router.use(accessControl);
 router.post('/ginprocess', updateGinProcessGreyoutStatusData);
+router.post('/spinprocess', updateSpinProcessGreyoutStatusData);
+router.post('/ginsales', updateGinSalesGreyoutStatusData);
 
 export default router;
