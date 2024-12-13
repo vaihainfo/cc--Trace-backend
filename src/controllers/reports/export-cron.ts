510cf741c207f3c5dcd52a6747d19d11ba3f7b22
padaliadhara <padalia.dhara@gmail.com>
2024-12-13 16:09:40 +0530

    village seed cotton allocation report

--- src/controllers/reports/export-cron.ts
+++ src/controllers/reports/export-cron.ts
@@ -96,6 +96,8 @@ const exportReportsOnebyOne = async () => {
   await generatePscpCottonProcurement();
   await generatePscpProcurementLiveTracker();
 
+  await exportVillageSeedCottonAllocation();
+
   // //brand wise report
   await generateBrandWiseData();
 
@@ -1919,6 +1921,119 @@ const generatePscpProcurementLiveTracker = async () => {
         // Rename the temporary file to the final filename
         fs.renameSync("./upload/pscp-procurement-sell-live-tracker-test.xlsx", './upload/pscp-procurement-sell-live-tracker.xlsx');
         console.log('Procurement report generation completed.');
+      })
+      .catch(error => {
+        console.log('Failed generation?.');
+        throw error;
+      });
+
+  } catch (error: any) {
+    console.error("Error appending data:", error);
+  }
+};
+
+const exportVillageSeedCottonAllocation = async () => {
+  const maxRowsPerWorksheet = 500000;
+ 
+  try {
+
+    const workbook = new ExcelJS.stream.xlsx.WorkbookWriter({
+      stream: fs.createWriteStream("./upload/village-seed-cotton-allocation-test.xlsx")
+    });
+    let worksheetIndex = 1;
+    let Count = 0;
+
+    const data = await sequelize.query(
+            `SELECT 
+            "gv"."village_id" AS "village_id", 
+            "farmer->village"."village_name" AS "village_name", 
+            "season"."id" AS "season_id", 
+            "season"."name" AS "season_name", 
+            "gn"."name" as "ginner_name",
+            "bk"."block_name" as "block_name",
+            "ds"."district_name" as "district_name",
+            COALESCE(SUM(CAST("farms"."total_estimated_cotton"AS DOUBLE PRECISION)), 0) AS "estimated_seed_cotton", 
+            COALESCE(SUM(CAST("farms"."cotton_transacted" AS DOUBLE PRECISION)), 0) AS "procured_seed_cotton", 
+            (COALESCE(SUM(CAST("farms"."total_estimated_cotton" AS DOUBLE PRECISION)), 0) - COALESCE(SUM(CAST("farms"."cotton_transacted" AS DOUBLE PRECISION)), 0)) AS "avaiable_seed_cotton" 
+            FROM "ginner_allocated_villages" as gv
+            LEFT JOIN 
+                "villages" AS "farmer->village" ON "gv"."village_id" = "farmer->village"."id" 
+            LEFT JOIN 
+                "farmers" AS "farmer" ON "farmer->village"."id" = "farmer"."village_id" 
+            LEFT JOIN 
+                "farms" as "farms" on farms.farmer_id = "farmer".id and farms.season_id = gv.season_id
+            LEFT JOIN 
+                "seasons" AS "season" ON "gv"."season_id" = "season"."id"
+            LEFT JOIN 
+                "ginners" AS gn ON "gn"."id" = "gv"."ginner_id" 
+            LEFT JOIN 
+                "blocks" AS bk ON "bk"."id" = "farmer->village"."block_id"
+            LEFT JOIN 
+                "districts" AS ds ON "ds"."id" = "bk"."district_id"
+            GROUP BY 
+                "gv"."village_id", "farmer->village"."id", "season"."id","gn".id,"bk".id,"ds".id
+            ORDER BY "gv"."village_id" DESC 
+        `,
+        {
+         type: sequelize.QueryTypes.SELECT
+        }
+      );
+
+      if (Count === maxRowsPerWorksheet) {
+        worksheetIndex++;
+        Count = 0;
+      }
+
+      let index = 0;
+      for await (const obj of data) {
+        const rowValues = Object.values({
+          index: index + 1,
+          village_name: obj.village_name ? obj?.village_name : "",
+          ginner_name: obj.ginner_name ? obj?.ginner_name : "",
+          block_name: obj.block_name ? obj?.block_name : "",
+          district_name: obj.district_name ? obj?.district_name : "",
+          season_name: obj.season_name ? obj?.season_name : "",
+          estimated_seed_cotton: obj.estimated_seed_cotton ? obj?.estimated_seed_cotton : "",
+          procured_seed_cotton: obj.procured_seed_cotton ? obj?.procured_seed_cotton : "",
+          avaiable_seed_cotton: obj?.avaiable_seed_cotton && obj?.avaiable_seed_cotton > 0 ? Number(obj?.avaiable_seed_cotton): 0,
+          prct_procured_cotton: Number(obj?.estimated_seed_cotton) > Number(obj?.procured_seed_cotton) ? Number(formatDecimal((Number(obj?.procured_seed_cotton) / Number(obj?.estimated_seed_cotton)) * 100) ): 0,
+        });
+        index++;
+
+        let currentWorksheet = workbook.getWorksheet(`Sheet${worksheetIndex}`);
+        if (!currentWorksheet) {
+          currentWorksheet = workbook.addWorksheet(`Sheet${worksheetIndex}`);
+          if (worksheetIndex == 1) {
+            currentWorksheet.mergeCells("A1:J1");
+            const mergedCell = currentWorksheet.getCell("A1");
+            mergedCell.value = "CottonConnect | Village Seed Cotton Allocation Report";
+            mergedCell.font = { bold: true };
+            mergedCell.alignment = { horizontal: "center", vertical: "middle" };
+          }
+          // Set bold font for header row
+          const headerRow = currentWorksheet.addRow([
+            "Sr No.",
+            "Village Name ",
+            "Ginner Name ",
+            "Block Name ",
+            "District Name ",
+            "Season ",
+            "Total Estimated Seed cotton of village (Kgs)",
+            "Total Seed Cotton Procured from village (Kgs)",
+            "Total Seed Cotton in Stock at village (Kgs)",
+            "% Seed Cotton Procured",
+          ]);
+          headerRow.font = { bold: true };
+        }
+        currentWorksheet.addRow(rowValues).commit();
+      }
+    
+
+    await workbook.commit()
+      .then(() => {
+        // Rename the temporary file to the final filename
+        fs.renameSync("./upload/village-seed-cotton-allocation-test.xlsx", './upload/village-seed-cotton-allocation.xlsx');
+        console.log('Village Seed Cotton Allocation Report completed.');
       })
       .catch(error => {
         console.log('Failed generation?.');
