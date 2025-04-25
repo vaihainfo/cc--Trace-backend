94ca85ca2f1d67f3e0d100649fa7df49ba7d7834
padaliadhara <padalia.dhara@gmail.com>
2025-04-25 12:18:04 +0530

    #10 correction in send_mail

--- src/controllers/send-emails/index.ts
+++ src/controllers/send-emails/index.ts
@@ -623,9 +623,15 @@ export const send_spin_mail = async (salesId: number) => {
                         model: Knitter,
                         as: "knitter",
                     },
-                   
+                    {
+                        model: Weaver,
+                        as: "weaver",
+                    },
+                    {
+                        model: Trader,
+                        as: "trader",
+                    }
                 ],
-               
                 
             }); 
 
